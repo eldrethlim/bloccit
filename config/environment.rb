00ef62298efdd651e881e74b760a56552fc87839
JunXian <eldrethlim@gmail.com>
2014-06-20 15:11:46 +0000

    sendgrid settings on environment.rb test

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,16 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Bloccit::Application.initialize!
+
+if Rails.env.production?
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    address:              'smtp.sendgrid.net',
+    port:                 '587', 
+    authentication:       :plain,
+    user_name:            ENV['SENDGRID_USERNAME'],
+    password:             ENV['SENDGRID_PASSWORD'],
+    domain:               'heroku.com',
+    enable_starttls_auto: true
+  }
+end
