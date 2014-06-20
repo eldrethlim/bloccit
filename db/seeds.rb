a57a3c64177913ef84bdd82eb10039f9bc23009e
JunXian <eldrethlim@gmail.com>
2014-06-20 03:15:27 +0000

    Associated users and posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,23 @@
 
 require 'faker'
 
+# Create Users
+5.times do
+	user = User.new(
+		name: 		Faker::Name.name,
+		email: 		Faker::Internet.email,
+		password: 	Faker::Lorem.characters(10)
+	)
+	user.skip_confirmation!
+	user.save
+end
+
+users = User.all
+
 # Create Posts
 50.times do 
   Post.create(
+  	user: users.sample,
     title: Faker::Lorem.sentence,
     body: Faker::Lorem.paragraph
     )
@@ -20,6 +34,12 @@ posts = Post.all
     )
 end
 
+User.first.update_attributes(
+	email: 'youremail.com',
+	password: 'helloworld',
+)
+
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
