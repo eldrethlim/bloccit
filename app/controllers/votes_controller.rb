661b473e564fab45b15ccbb351870c60c066f952
JunXian <eldrethlim@gmail.com>
2014-06-28 11:14:22 +0000

    Refactored votes controller

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,69 +2,67 @@
 
 class VotesController < ApplicationController
 
+before_action :load_vote_from_post, only: [:p_up_vote, :p_down_vote]
+before_action :load_vote_from_comment, only: [:c_up_vote, :c_down_vote]
+
   def p_up_vote
-    p_vote_setup
-    p_update_vote(1)
+    update_vote(1, {post: @post})
 
     redirect_to :back
   end
 
   def p_down_vote
-    p_vote_setup 
-    p_update_vote(-1)
+    update_vote(-1, {post: @post})
 
     redirect_to :back
   end
 
   def c_up_vote
-    c_vote_setup
-    c_update_vote(1)
+    update_vote(1, {comment: @comment})
 
     redirect_to :back
   end
 
   def c_down_vote
-    c_vote_setup
-    c_update_vote(-1)
+    update_vote(-1, {comment: @comment})
 
     redirect_to :back
   end
 
   private
 
-  def p_update_vote(new_value)
+  def update_vote(new_value, build_args)
     if @vote
       authorize @vote, :update?
       @vote.update_attribute(:value, new_value)
     else
-      @vote = current_user.votes.build(value: new_value, post: @post)
+      @vote = current_user.votes.build({value: new_value}.merge(build_args))
       authorize @vote, :create?
       @vote.save
     end
   end
 
-  def c_update_vote(new_value)
-    if @vote
-      authorize @vote, :update?
-      @vote.update_attribute(:value, new_value)
-    else
-      @vote = current_user.votes.build(value: new_value, comment: @comment)
-      authorize @vote, :create?
-      @vote.save
-    end
-  end
-
-  def p_vote_setup
-    @topic = Topic.find(params[:topic_id])
-    @post = @topic.posts.find(params[:post_id])
-
-    @vote = @post.votes.where(user_id: current_user.id).first
-  end
-
-  def c_vote_setup
+  def load_post
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.find(params[:comment_id])
-
-    @vote = @comment.votes.where(user_id: current_user.id).first
   end
+
+  def load_comment
+    load_post
+    @comment = @post.comments.find(params[:comment_id])
+  end
+
+  def load_vote(parent)
+    @vote = parent.votes.where(user_id: current_user.id).first
+  end
+
+  def load_vote_from_post
+    load_post
+    load_vote(@post)
+  end
+
+  def load_vote_from_comment
+    load_post
+    load_vote(@comment)
+  end
+
 end
